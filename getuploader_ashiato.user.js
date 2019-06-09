780da993d8c033afd48e94c04dfd9abe14dd60d3
braveripple <36323215+braveripple@users.noreply.github.com>
2019-06-09 22:24:37 +0900

    Update getuploader_ashiato.user.js

--- getuploader_ashiato.user.js
+++ getuploader_ashiato.user.js
@@ -7,42 +7,88 @@
 // @match        https://*.getuploader.com/*
 // @grant        GM_getValue
 // @grant        GM_setValue
+// @grant        GM_addStyle
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/jspanel3/3.11.2/jquery.jspanel.min.js
+// @ran-at       document-end
 // ==/UserScript==
 
 (function() {
-    'use strict';
+  'use strict';
+  //document.head.insertAdjacentHTML('beforeend', `<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">`);
+  document.head.insertAdjacentHTML('beforeend', `<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jspanel3/3.11.2/jquery.jspanel.min.css" />`);
 
-    // 格納データ
-    // recentUploaderURL : 直前に訪問したアップローダーURL
-    // footprints : 過去に訪問したアップローダー情報
+  // 格納データ
+  // recentUploaderURL : 直前に訪問したアップローダーURL
+  // footprints : 過去に訪問したアップローダー情報
 
-    const recentUploaderURL = GM_getValue("recentUploaderURL","");
+  const recentUploaderURL = GM_getValue("recentUploaderURL","");
 
-    // アップローダーURLを取得
-    const uploaderName = location.pathname.match(/\/(\w+)\//)[1];
-    const uploaderURL = `${location.origin}/${uploaderName}`;
+  // アップローダーURLを取得
+  const uploaderPath = location.pathname.match(/\/(\w+)\/?/)[1];
+  const uploaderURL = `${location.origin}/${uploaderPath}`;
 
-    // アップローダーURLが直近のものだったら終了
-    if (uploaderURL === recentUploaderURL) { return; }
+  // アップローダーURLが直近のものだったら終了
+  //if (uploaderURL === recentUploaderURL) { return; }
 
-    // 過去の足跡に今回訪問したアップローダーURLがあれば削除
-    const footprints = JSON.parse(GM_getValue("footprints","[]"));
-    const index = footprints.findIndex(e => e.url === uploaderURL);
-    if (index !== -1) {
-        // 破壊的メソッドで取り出した後の配列を作るのが目的なので戻り値は不要。
-        footprints.splice(index, 1);
+  // 過去の足跡に今回訪問したアップローダーURLがあれば削除
+  const footprints = JSON.parse(GM_getValue("footprints","[]"));
+  const index = footprints.findIndex(e => e.url === uploaderURL);
+  if (index !== -1) {
+    // 破壊的メソッドで取り出した後の配列を作るのが目的なので戻り値は不要。
+    footprints.splice(index, 1);
+  }
+
+  // 足跡をつける
+  const uploaderName = document.querySelector(".page-header > h1").firstChild.nodeValue.trim();
+  const footprint = {
+    url : uploaderURL,
+    name : uploaderName,
+    timestamp : Date.now()
+  }
+  footprints.push(footprint);
+
+  GM_setValue("recentUploaderURL", uploaderURL);
+  GM_setValue("footprints", JSON.stringify(footprints));
+  console.log(footprints);
+  console.log(uploaderURL);
+
+  // ----------------------------------------------
+  var tmp = footprints.reverse().map((value, index) => `<button type="button" class="list-group-item list-group-item-action" data-url="${value.url}">${value.name}</button>`).join('');
+  var content = `
+<div class="list-group">
+  ${tmp}
+</div>`;
+  function clickButton(event) {
+    window.location.href = event.target.dataset.url;
+  }
+
+  const $ = window.jQuery;
+  $.jsPanel({
+    contentSize: { width: 300, height: 450 },
+    content: '<iframe width="100%" height="100%"></iframe>',
+    position: {
+      my: "left-center",
+      at: "left-center"
+    },
+    theme: 'rebeccapurple',
+    headerTitle: 'あしあと。',
+    callback: function (panel) {
+
+      const iframe = panel.content[0].firstElementChild;
+      const iDocument = iframe.contentDocument;
+      iDocument.head.insertAdjacentHTML('beforeend', `<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">`);
+      iDocument.open();
+      iDocument.write(content);
+      iDocument.close();
+      Array.from(iDocument.querySelectorAll("button")).forEach(function (element) {
+        element.addEventListener('click', clickButton);
+      });
+      // ↓この処理がないと画面表示直後、パネルを垂直方向にしか動かせない（バグ？）
+      panel.maximize();
+      panel.normalize();
     }
+  });
 
-    // 足跡をつける
-    const footprint = {
-        url : uploaderURL,
-        timestamp : Date.now()
-    }
-    footprints.push(footprint);
-
-    GM_setValue("recentUploaderURL", uploaderURL);
-    GM_setValue("footprints", JSON.stringify(footprints));
-    console.log(footprints);
-    console.log(uploaderURL);
 
 })();
